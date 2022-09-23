f58b5a1326f950a691d01eb7355f856474c5d1bc
William Ngo <williamkimngo@gmail.com>
2022-09-23 10:54:36 -0700

    first commit

---
+++ backend/routes/api/index.js
@@ -0,0 +1,43 @@
+const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+const { restoreUser } = require('../../utils/auth.js');
+
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+router.use(restoreUser);
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.get('/set-token-cookie', async (_req, res) => {
+  const user = await User.findOne({
+      where: {
+        username: 'Demo-lition'
+      }
+    });
+  setTokenCookie(res, user);
+  return res.json({ user });
+});
+
+router.get(
+  '/restore-user',
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+const { requireAuth } = require('../../utils/auth.js');
+router.get(
+  '/require-auth',
+  requireAuth,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
+router.post('/test', function(req, res) {
+  res.json({ requestBody: req.body });
+});
+
+
+module.exports = router;
