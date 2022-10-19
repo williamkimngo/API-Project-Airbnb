564d44bdb39eeb1f0488fd5b546607edc2d6a5a2
William Ngo <williamkimngo@gmail.com>
2022-10-19 14:36:04 -0700

    spots and spotsid basic css done

--- frontend/src/components/SpotDetail/index.js
+++ frontend/src/components/SpotDetail/index.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux"
 import { useParams, NavLink } from "react-router-dom"
 import { actionGetSpotReview } from "../../store/reviews"
 import { getOneSpot } from "../../store/spots"
+import './spotId.css';
 
 const SpotDetail = () => {
     const dispatch = useDispatch()
@@ -49,79 +50,101 @@ const SpotDetail = () => {
     }
     return (
         <div className="Spot-Detail-container">
+            <div className="header">
             <h1>{currentSpot.name}</h1>
+            </div>
             <div className="title-container">
                 <span> &#9733; </span>
-                <span> {avgRatingTwoDec} </span>
+                <span className="rating"> {avgRatingTwoDec} </span>
                 <span> · </span>
                 <span> {currentSpot.numReviews} reviews </span>
-                {currentSpot.city}, {currentSpot.state}, {currentSpot.country}
+                <span> · </span>
+                <span> 🎖️ </span>
+                <span>  Superhost </span>
+                <span> · </span>
+                <span className="location">{currentSpot.city}, {currentSpot.state}, {currentSpot.country}</span>
             </div>
             <div className="img-container">
 
                 <img className="first-img" src={currentSpot?.SpotImages[0].url} alt='SpotImage'/>
             </div>
-            <div className="Spot-detail-description-container">
-                <h2 className="spot-detail-name">{currentSpot.name} hosted by {currentSpot.Owner?.firstName}</h2>
 
-            </div>
-            <div className="spot-detail-name">{currentSpot.description}</div>
-            <div className="Second Container Space"></div>
-            <div className="spot-detail-price container">
+              <div className="big-middle-div">
+              <div className="Spot-detail-description-container">
+              <h2 className="spot-detail-name">{currentSpot.name} hosted by {currentSpot.Owner?.firstName}</h2>
+              <div className="extra-info">
+                <div>
+                    <div className="info-title">🚪Self Check in
+                    <p>Check yourself in with the lockbox.</p>
+                    </div>
+
+                </div>
+                <div>
+                    <div className="info-title"> 🎖️{currentSpot.Owner.firstName} is a Superhost
+                    <p>Superhosts are experienced, highly rated hosts who are committed to providing great stays for guests.</p>
+                    </div>
+
+                </div>
+                <div className="info-title">📆 Free Cancellation within 48 hours of your expected arrival</div>
+              </div>
+              <p className="spot-detail-description-left">{currentSpot.description}</p>
+              </div>
+            <div className="Right-Container">
+            <div className="spot-detail-price-container">
                 <span>
                     <span className="spot-detail-price">${currentSpot.price}</span>
-                    <span>per night</span>
+                    <span className="per-night">per night</span>
                 </span>
                 <span className="price-with-review-top-right">
                     <span id='next-to-right'> &#9733; </span>
                     <span> {avgRatingTwoDec} · {currentSpot.numReviews} reviews</span>
                 </span>
             </div>
+
             <div>
                 <div className="price-line-container">
-                    <span>
-                        <span>${currentSpot.price}</span>
-                        <span> x3 nights</span>
-                    </span>
-                    <span>${currentSpot.price * 3}</span>
+                    <div>
+                        <div className="left-price">${currentSpot.price} x3 nights</div>
+                    </div>
+                    <div className="right-price">${currentSpot.price * 3}</div>
                 </div>
                 <div className="price-line-container">
-                    <span>Cleaning Fee</span>
-                    <span>${currentSpot.price / 5}</span>
+                    <div className="left-price">Cleaning Fee</div>
+                    <div className="right-price">${currentSpot.price / 5}</div>
                 </div>
                 <div className="price-line-container">
-                    <span>Service Fee</span>
-                    <span>${currentSpot.price / 10}</span>
+                    <div className="left-price">Service Fee</div>
+                    <div className="right-price">${currentSpot.price / 10}</div>
                 </div>
-                <div className="price-line-container">
-                    <span>Total before taxes</span>
-                    <span>${(currentSpot.price * 3) + (currentSpot.price / 5) + (currentSpot.price / 10)}</span>
+                <div className="total-price-line-container">
+                    <div>Total before taxes</div>
+                    <div>${(currentSpot.price * 3) + (currentSpot.price / 5) + (currentSpot.price / 10)}</div>
+                </div>
+
                 </div>
             </div>
-            <div className="spot-detail-review-container"></div>
-            <div>
+            </div>
+
+
+            <div className="spot-detail-review-container">
                 <h2>
                 <span> &#9733; </span>
                 <span> {avgRatingTwoDec} </span>
                 <span> · </span>
                 <span> {currentSpot.numReviews} reviews </span>
                 </h2>
-                {allowCreate && <NavLink to={`/spots/${spotId}/reviews/new`}> Leave a Review</NavLink>}
+                </div>
+                <div className="review-link">
+                {allowCreate && <NavLink to={`/spots/${spotId}/reviews/new`}> Leave a Review</NavLink>}</div>
                 {spotReview.map(review => (
                     <div className='single-review-container' key={review.id}>
                         <div className='review-name'>{review?.User?.firstName || "You Just posted"}</div>
-                        <div className='review-date'>{review?.createdAt.slice(0, 7)}</div>
+                        <div className='review-date'>{review?.createdAt.slice(0, 9)}</div>
                         <div className='review-text'>{review.review}</div>
-                        <div>{review.stars} star</div>
+                        <div>{review.stars} &#9733;</div>
                         </div>
-
                 ))}
 
-            </div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
         </div>
     )
 
